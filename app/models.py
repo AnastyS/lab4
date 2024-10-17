a6ec15b3835bb976c314aa45df8630c85481f001
AnastyS <nastya.samokhval.05@bk.ru>
2024-10-17 09:06:59 +0500

    new

--- app/models.py
+++ app/models.py
@@ -1,10 +1,16 @@
 # Импорт модуля models из библиотеки Django
 from django.db import models
+from django.core.exceptions import ValidationError
 # Определение класса, который наследует модель Django Model
 class Student(models.Model):
     name = models.CharField(max_length=100)
     subject = models.CharField(max_length=100)
     grade = models.IntegerField()
+
+    def clean(self):
+        # Проверка, что оценка находится в диапазоне от 2 до 5
+        if self.grade < 2 or self.grade > 5:
+            raise ValidationError('Оценка должна быть в диапазоне от 2 до 5.')
 
     # Определение метода __str__, который будет использоваться для представления экземпляров модели в виде строки
     def __str__(self):
